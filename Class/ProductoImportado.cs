742f6047385f20f2037a7aaee5a164d63879c159
raide <raide@LAPTOP-DOFK9MS3>
2022-10-07 23:29:22 -0400

    Add project files.

---
+++ Class/ProductoImportado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Producto
+{
+      class ProductImported : Product
+    {
+        protected float tax;
+
+        public ProductImported(string pDescription = "Default", float pPurchase = 0,float pTax=0) 
+            : base(pDescription,pPurchase)
+        {
+
+            tax = pTax;
+            CalculPrice();
+
+            }
+
+        public float Tax
+        { 
+            get { return tax; } 
+            set { tax = value;
+                CalculPrice(); } 
+        }
+
+        public override void CalculPrice()
+        {
+            
+            price_sale = (price_purchase + tax) * 2;
+        }
+
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Producto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                ProductProduced r = new ProductProduced("J", 100, 6);
+                ProductImported p = new ProductImported("R", 2100, 8);
+            try
+            {
+                p.PricePurchase = -100;
+
+                r.Show();
+                p.Show();
+               
+
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Out Of Memory, please use a number in the range >0 and <1000000\n");
+            }
+
+            r.Show();
+            p.Show();
+            
+        }
+    }
+}
