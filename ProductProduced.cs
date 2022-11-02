6bce1f070979710017bb5bca82ba7af954d9c60b
raidel2808 <114402417+raidel2808@users.noreply.github.com>
2022-11-02 15:08:24 -0400

    Add files via upload

---
+++ ProductProduced.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Producto
+{
+     class ProductProduced : ProductImported
+    {
+        public float cost;
+
+        public ProductProduced(string pDescription = "Default",
+            float pPurchase = 0, float pTax = 0, float pCost=0)
+            : base(pDescription, pPurchase, pTax)
+        {
+            cost = pCost;
+            CalculPrice();
+            
+        }
+
+        public float Cost
+            { 
+            get { return cost; } 
+            set { cost = value;
+                CalculPrice();
+            } 
+        }
+        public override void CalculPrice()
+        {
+            
+            price_sale = (price_purchase + tax + cost) * 2;
+        }
+
+       
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1_Producto
+{
+    public class Product
+    {
+        private string description;
+        protected float price_purchase;
+        protected float price_sale;
+
+        public Product(string pDescription = "Default", float pPurchase = 0)
+        {
+
+
+            description = pDescription;
+            price_purchase = pPurchase;
+            CalculPrice();
+
+
+
+
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public float PricePurchase
+        {
+            get { return price_purchase; }
+            set {
+                if (value < 0 || value > 1000000)
+                {
+                    throw new OutOfMemoryException();
+                }
+                price_purchase = value;
+                CalculPrice();
+            }
+        }
+
+        public float SalePrice
+        {
+            get { return price_sale; }
+
+        }
+
+        public virtual void CalculPrice()
+        {
+            
+            price_sale = price_purchase * 2;
+
+        }
+
+        public static int Gain(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+
+
+        public void Show()
+        {
+            Console.WriteLine("Description of the product: {0}\nPrice of purchase: {1}\nPrice of sale: {2}\n---------", description,price_purchase, price_sale);
+        }
+    }
+}
